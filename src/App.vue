ff0d71b8786d8817413c227499cd42f0a4d6ea9e
Luana <lcorreia@vntrx.com>
2022-11-01 23:06:41 -0300

    feat: getters, favs tasks and counter

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,22 @@
       <img src="@/assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia Tasks</h1>
     </header>
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
 
     <!-- task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
-        <!-- <p>{{ task.title }}</p> -->
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favCount }} tasks in your favs list.</p>
+      <div v-for="task in taskStore.favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -19,12 +30,14 @@
 <script>
 import { useTaskStore } from "./stores/TaskStore";
 import TaskDetails from "@/components/TaskDetails.vue";
+import { ref } from "@vue/reactivity";
 
 export default {
   components: { TaskDetails },
   setup() {
     const taskStore = useTaskStore();
-    return { taskStore };
+    const filter = ref("all");
+    return { taskStore, filter };
   },
 };
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -8,4 +8,17 @@ export const useTaskStore = defineStore("taskStore", {
     ],
     name: "Yoshi",
   }),
+  getters: {
+    favs() {
+      return this.tasks.filter((t) => t.isFav);
+    },
+    favCount() {
+      return this.tasks.reduce((p, c) => {
+        return c.isFav ? p + 1 : p;
+      }, 0);
+    },
+    totalCount: (state) => {
+      return state.tasks.length;
+    },
+  },
 });
