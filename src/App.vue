40aeb4e97060a2e5fcf9566dec5e920248fd3f9d
Luana <lcorreia@vntrx.com>
2022-11-01 22:05:36 -0300

    feat: lesson 4 - task layout and access task store

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,26 @@
       <img src="@/assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia Tasks</h1>
     </header>
+
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <!-- <p>{{ task.title }}</p> -->
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import { useTaskStore } from "./stores/TaskStore";
+import TaskDetails from "@/components/TaskDetails.vue";
+
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore();
+    return { taskStore };
+  },
+};
 </script>
